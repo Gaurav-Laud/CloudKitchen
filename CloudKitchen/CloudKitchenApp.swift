91c4b13d22f81155ddada6b98bb9b2613d6e3324
Gaurav Laud <gaurav.laud@innovapptive.com>
2024-06-30 20:30:22 +0530

    Initial Commit

---
+++ CloudKitchen/CloudKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitchenApp.swift
+//  CloudKitchen
+//
+//  Created by E5000745 on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

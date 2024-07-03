b6facb2ccd935abd0c26dfdffbdf495a957a700b
Gaurav Laud <gauravlaud@gmail.com>
2024-07-04 01:10:30 +0530

    Added constants for LoginView

--- CloudKitchen/Utility/Constants.swift
+++ CloudKitchen/Utility/Constants.swift
@@ -14,13 +14,27 @@ struct Constants {
     static let home_made_kitchens = "Home-Made\nKitchens"
     static let high_quality = "High Quality"
     static let cost_efficient = "Cost Efficient"
+    static let login = "Login"
+    static let enter_your_phone_number_to_proceed = "Enter your phone number to proceed"
+    static let phone_number = "Phone Number"
+    static let authenticate = "AUTHENTICATE"
+    static let trouble_login = "Trouble Login?"
+    static let terms_conditions = "Terms & Conditions"
+    static let privacy_policy = "Privacy Policy"
+    static let country_code = "+91"
     
     // Resource names
     static let WelcomeBackgroundImage = "WelcomeBackgroundImage"
     static let hot_food1 = "hot-food1"
     static let guarantee = "Guarantee"
     static let expensive_price = "Expensive Price"
+    static let login_screen_image = "LoginScreenImage"
+    static let india_flag = "IndiaFlag"
     
     // Font names
     static let times_new_roman = "Times New Roman"
+    
+    // UserDefaults keys
+    static let isSignedIn = "isSignedIn"
+    static let accessToken = "accessToken"
 }

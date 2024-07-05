47bdf971f3952518aca5e430f8f3ae076238db8b
Gaurav Laud <gauravlaud@gmail.com>
2024-07-05 22:51:10 +0530

    Added HomeView

---
+++ CloudKitchen/Modules/HomeModule/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  CloudKitchen
+//
+//  Created by E5000745 on 05/07/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}

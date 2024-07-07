66136e3f8482fe1b7d74bb4ad5136b4cc90c8378
Gaurav Laud <gauravlaud@gmail.com>
2024-07-07 12:45:30 +0530

    Added navigation to home from OTPVerification

--- CloudKitchen/Modules/LoginModule/Views/OTPVerificationScreen.swift
+++ CloudKitchen/Modules/LoginModule/Views/OTPVerificationScreen.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import OtpView_SwiftUI
 struct OTPVerificationScreen: View {
     @ObservedObject private var loginViewModel: LoginViewModel
+    @State private var navigateToHome: Bool = false
     init(loginViewModel: LoginViewModel) {
         self.loginViewModel = loginViewModel
     }
@@ -39,23 +40,28 @@ struct OTPVerificationScreen: View {
                 self.resendButton(isEnabled: $loginViewModel.timer.wrappedValue == nil, time: loginViewModel.minTimeLimit - $loginViewModel.time.wrappedValue)
             }
             CloudButton(title: Constants.verify, isTextCaps: true) {
-                loginViewModel.verifyOTP(loginViewModel.otp) { isOTPVerified in
-                    
+                loginViewModel.verifyOTP(loginViewModel.otp) { result in
+                    switch result {
+                    case .success(let isVerified):
+                        navigateToHome = isVerified
+                    case .failure(_): break
+                    }
                 }
             }
+            .navigationDestination(isPresented: $navigateToHome, destination: { HomeView() })
         }
     }
     @ViewBuilder
     private func getOTPView() -> some View {
         HStack {
-            CloudLabel(text: "OTP", font: .title3, textColor: .red, fontWeight: .heavy)
+            CloudLabel(text: Constants.OTP, font: .title3, textColor: .red, fontWeight: .heavy)
             OtpView_SwiftUI(otpCode: $loginViewModel.otp, otpCodeLength: 4, textColor: .red, textSize: 20)
                 .frame(width: 250)
         }
     }
     @ViewBuilder
     private func resendButton(isEnabled: Bool, time: Int) -> some View {
-        Text(isEnabled ? "Resend OTP?" : "Resend OTP in \(time)")
+        Text(isEnabled ? Constants.resend_OTP : "\(Constants.resend_OTP_in) \(time)")
             .font(.body)
             .foregroundStyle(isEnabled ? .black : .secondary)
             .onTapGesture {

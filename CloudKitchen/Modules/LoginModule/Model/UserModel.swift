32a377e0e6f4c73148e75891862a8641e4df2b4d
Gaurav Laud <gauravlaud@gmail.com>
2024-07-07 12:41:36 +0530

    Added name to userModel

--- CloudKitchen/Modules/LoginModule/Model/UserModel.swift
+++ CloudKitchen/Modules/LoginModule/Model/UserModel.swift
@@ -11,4 +11,14 @@ class UserModel: Codable {
     var mobileNumber: String
     var isAppAdmin: Bool
     var isKitchenOwner: Bool
+    var name: String
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decodeIfPresent(String.self, forKey: .id) ?? ""
+        self.mobileNumber = try container.decodeIfPresent(String.self, forKey: .mobileNumber) ?? ""
+        self.isAppAdmin = try container.decodeIfPresent(Bool.self, forKey: .isAppAdmin) ?? false
+        self.isKitchenOwner = try container.decodeIfPresent(Bool.self, forKey: .isKitchenOwner) ?? false
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+    }
 }

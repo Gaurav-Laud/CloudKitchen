2a4a97398aa62fcb970297f45c91d9de23e62151
Gaurav Laud <gauravlaud@gmail.com>
2024-07-09 15:29:51 +0530

    Added kitchenCellView for home view

---
+++ CloudKitchen/Modules/HomeModule/View/KitchenCellView.swift
@@ -0,0 +1,60 @@
+//
+//  KitchenCellView.swift
+//  CloudKitchen
+//
+//  Created by E5000745 on 07/07/24.
+//
+
+import SwiftUI
+
+struct KitchenCellView: View {
+    var kitchenModel: KitchenModel
+    var body: some View {
+            VStack {
+                AsyncImage(url: URL(string: "\(kitchenModel.bannerImage)"), content: { image in
+                    image.resizable()
+                }, placeholder: {
+                    ProgressView()
+                })
+                    .frame(height: 180)
+                getBottomView()
+                    .padding()
+            }
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .shadow(radius: 5)
+            .padding()
+    }
+    @ViewBuilder
+    private func getBottomView() -> some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading) {
+                CloudLabel(text: self.kitchenModel.name, font: .title,textColor: .yellow, fontWeight: .bold)
+                Text(self.kitchenModel.description)
+            }
+            Spacer()
+            VStack(alignment: .trailing) {
+                getRatingView(rating: kitchenModel.avgRating)
+                CloudLabel(text: "\(Constants.starts_from) \(kitchenModel.startingPrice)",textColor: .secondary)
+                CloudLabel(text: "Delivery in 21 min")
+            }
+        }
+    }
+    @ViewBuilder
+    private func getRatingView(rating: Float) -> some View {
+        HStack {
+            Text(String(format: "%0.1f", rating))
+                .foregroundStyle(.white)
+            Image(systemName: Constants.star_fill)
+                .foregroundStyle(.white)
+        }
+        .padding(.horizontal, 7)
+        .padding(.vertical, 5)
+        .background(.green)
+        .clipShape(Capsule())
+    }
+}
+
+//#Preview {
+//    KitchenCellView()
+//}

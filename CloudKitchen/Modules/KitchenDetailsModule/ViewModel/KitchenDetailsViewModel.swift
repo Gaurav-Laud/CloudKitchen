d1b44a836565c27065ac5f8c06789da51a068519
Gaurav Laud <gauravlaud@gmail.com>
2024-07-11 23:59:53 +0530

    Implemented fetch API call for KitchenDetails

--- CloudKitchen/Modules/KitchenDetailsModule/ViewModel/KitchenDetailsViewModel.swift
+++ CloudKitchen/Modules/KitchenDetailsModule/ViewModel/KitchenDetailsViewModel.swift
@@ -8,5 +8,17 @@
 import Foundation
 class KitchenDetailsViewModel: ObservableObject {
     @Published var kitchenModel: KitchenModel?
-//    @Published var kitchenDetailsModel: KitchenDetailsModel?
+    @Published var kitchenDetailsModel: KitchenDetailsModel?
+    
+    func fetchKitchenDetails(with kitchenId: String) {
+        let url = "https://whale-app-ct2dl.ondigitalocean.app/kitchens/\(kitchenId)"
+        Task { @MainActor [weak self] in
+            guard let self = self else { return }
+            do {
+                self.kitchenDetailsModel = try await APIHandler.shared.makeFetchAPICall(KitchenDetailsModel.self, url: url)
+            } catch {
+                print("error while fetching kitchen details: \(error)")
+            }
+        }
+    }
 }

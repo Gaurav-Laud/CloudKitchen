2d77a5191dd63c4ee3e7e08b4a023311a3e26213
Gaurav Laud <gauravlaud@gmail.com>
2024-07-18 10:20:31 +0530

    Integrated AddressManagementView with HomeView

--- CloudKitchen/Modules/HomeModule/View/HomeView.swift
+++ CloudKitchen/Modules/HomeModule/View/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     @StateObject private var homeViewModel = HomeViewModel()
+    @State private var showAddressManagementView = false
     var body: some View {
         NavigationStack {
             VStack {
@@ -28,6 +29,7 @@ struct HomeView: View {
                 .scrollIndicators(.never)
                 Spacer()
             }
+            .navigationDestination(isPresented: $showAddressManagementView, destination: { AddressManagementView() })
             .toolbarRole(.navigationStack)
             .toolbar {
                 getToolBarView()
@@ -38,17 +40,23 @@ struct HomeView: View {
     }
     @ToolbarContentBuilder
     private func getToolBarView() -> some ToolbarContent {
-        ToolbarItem(placement: .topBarLeading, content: {
+        ToolbarItem(placement: .topBarLeading) {
             Image(Constants.home_heart, bundle: Bundle.main)
                 .resizable()
                 .frame(width: 31, height: 31)
-        })
-        ToolbarItem(placement: .topBarLeading, content: {
+                .onTapGesture {
+                    showAddressManagementView = true
+                }
+        }
+        ToolbarItem(placement: .topBarLeading) {
             VStack(content: {
-                Text("\(homeViewModel.selectedLocation?.addressLine1 ?? "")")
-                Text("\(homeViewModel.selectedLocation?.addressLine2 ?? "")")
+                Text("\(CloudKitchenUtility.shared.selectedAddress?.houseNo ?? "")")
+                Text("\(CloudKitchenUtility.shared.selectedAddress?.addressLine1 ?? "")")
             })
-        })
+            .onTapGesture {
+                showAddressManagementView = true
+            }
+        }
     }
 }
 

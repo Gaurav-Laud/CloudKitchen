699be15b4cfbbf8c4705cda1a669f7e8d4939dc1
Gaurav Laud <gauravlaud@gmail.com>
2024-08-24 19:04:47 +0530

    Implmented OrderConfirmationView

---
+++ CloudKitchen/Modules/OrderConfirmationModule/View/OrderConfirmationView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderConfirmationView.swift
+//  CloudKitchen
+//
+//  Created by E5000745 on 22/08/24.
+//
+
+import SwiftUI
+
+struct OrderConfirmationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderConfirmationView()
+}
---
+++ CloudKitchen/Modules/OrderConfirmationModule/ViewModel/OrderConfirmationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OrderConfirmationViewModel.swift
+//  CloudKitchen
+//
+//  Created by E5000745 on 23/08/24.
+//
+
+import Foundation

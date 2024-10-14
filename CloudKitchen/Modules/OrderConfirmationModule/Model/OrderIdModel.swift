c3e51bd23d4cef749548db198ded503b335b9da1
Gaurav Laud <gauravlaud@gmail.com>
2024-10-14 20:55:29 +0530

    Added the OrderIdModel

--- CloudKitchen/Modules/OrderConfirmationModule/Model/OrderIdModel.swift
+++ CloudKitchen/Modules/OrderConfirmationModule/Model/OrderIdModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+class OrderIdModel: Codable {
+    var amount: Int
+    var amount_due: Int
+    var amount_paid: Int
+    var attempts: Int
+    var created_at: Double
+    var currency: String
+    var entity: String
+    var id: String
+    var notes: [String: String]
+    var offer_id: String?
+    var receipt: String
+    var status: String
+}
